d0bd9d1b92fbb9f3a08ef188ee8032533b0cfcb3
really-no-name <xu_bolun@126.com>
2024-02-22 14:22:11 +0000

    improve model

--- CNN_cpu.py
+++ CNN_cpu.py
@@ -27,16 +27,38 @@ test_dataloader = DataLoader(test_data, batch_size=64)
 class Tudui(nn.Module):
     def __init__(self):
         super(Tudui, self).__init__()
+        # self.model = nn.Sequential(
+        #     nn.Conv2d(3, 32, 5, 1, 2),
+        #     nn.MaxPool2d(2),
+        #     nn.Conv2d(32, 32, 5, 1, 2),
+        #     nn.MaxPool2d(2),
+        #     nn.Conv2d(32, 64, 5, 1, 2),
+        #     nn.MaxPool2d(2),
+        #     nn.Flatten(),
+        #     nn.Linear(64*4*4, 64),
+        #     nn.Linear(64, 10)
+        # )
         self.model = nn.Sequential(
-            nn.Conv2d(3, 32, 5, 1, 2),
-            nn.MaxPool2d(2),
-            nn.Conv2d(32, 32, 5, 1, 2),
-            nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, 5, 1, 2),
-            nn.MaxPool2d(2),
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
             nn.Flatten(),
-            nn.Linear(64*4*4, 64),
-            nn.Linear(64, 10)
+            nn.Linear(256 * 4 * 4, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 10)
         )
 
     def forward(self, x):
